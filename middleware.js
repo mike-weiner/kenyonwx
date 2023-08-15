33f1da0c35a54b3673a2050977633699da40df6e
Michael Weiner <vikings412@gmail.com>
2023-08-14 20:51:44 -0500

    restore fixed middleware

---
+++ middleware.js
@@ -0,0 +1,9 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req, ev) {
+    if (process.env.NODE_ENV === 'production' && !req.headers.get('x-forwarded-proto').includes('https')) {
+        return NextResponse.redirect(`https://${req.headers.get('host')}${req.nextUrl.pathname}`, 301);
+    }
+
+    return NextResponse.next();
+}
