00aa4bc935772a11d41c10d300487107b018cdcf
Michael Weiner <vikings412@gmail.com>
2023-08-14 20:09:14 -0500

    enhanced error handling with improved error reporting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
 
 import StatPack from '../components/Packs/StatPack';
 import BottomBanner from '../components/BottomBanner';
+import FailureDialog from '../components/Dialog/Nondismissable/Failure';
 
 const axios = require('axios');
 const memjs = require('memjs');
@@ -20,6 +20,10 @@ export default function Homepage(props) {
   const [precipData, setPrecipData] = useState([]);
 
   useEffect(() => {
+    if (props.error.title !== "") {
+      return;
+    }
+
     setPressureData(
       [
         { name: 'Absolute Pressure', stat: props.wxData.bar_absolute + '"', includeTrend: true, change: props.wxData.bar_trend, changeType: props.wxData.bar_trend >= 0 ? 'increase' : 'decrease' },
@@ -69,8 +73,17 @@ export default function Homepage(props) {
         <meta name="twitter:card" content="summary"></meta>
       </Head>
 
-      <div className="p-8 pb-24 space-y-8">
+      {
+        props.error.title !== "" ?
+          <FailureDialog
+            title={props.error.title || "An Error Occurred"}
+            msg={props.error.msg || "An error has occurred. Please contact a site administrator."}
+          />
+          :
+          null
+      }
 
+      <div className="p-8 pb-24 space-y-8">
         <div className="md:flex md:items-center md:justify-between">
           <div className="min-w-0 flex-1">
             <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
@@ -115,7 +128,12 @@ export default function Homepage(props) {
           title="Precipitation"
         />
 
-        <BottomBanner msg={"Weather Measurements Taken @ " + props.wxData.generated_at} />
+        {
+          !fetchingData ?
+            <BottomBanner msg={"Weather Measurements Taken @ " + props.wxData.generated_at} />
+            :
+            null
+        }
 
       </div>
     </>
@@ -124,50 +142,103 @@ export default function Homepage(props) {
 
 export async function getServerSideProps(context) {
 
-  var mc = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
+  const KW_LOG_PREFIX = "[KW]: ";
+  const KW_MEMCACHED_KEY = "kwx-data";
+  const KW_MEMCACHED_TIMEOUT_DURATION_IN_SECONDS = 300;
+
+  let error = {
+    title: "",
+    msg: "",
+  };
+
+  let mc = memjs.Client.create(process.env.MEMCACHEDCLOUD_SERVERS, {
     username: process.env.MEMCACHEDCLOUD_USERNAME,
     password: process.env.MEMCACHEDCLOUD_PASSWORD,
   });
 
-  const KW_MEMCACHED_KEY = "kwx-data";
-  const KW_MEMCACHED_TIMEOUT_DURATION_IN_SECONDS = 300;
-
   let dataInCache = false;
 
-  var { err, value } = await mc.get(KW_MEMCACHED_KEY);
-  if (err == null && value != null) {
-    dataInCache = true;
+  try {
+    let { value } = await mc.get(KW_MEMCACHED_KEY);
+    if (value != null) {
+      dataInCache = true;
+
+      let parsedCacheData = JSON.parse(value.toString('utf-8'));
+
+      return {
+        props: {
+          wxData: parsedCacheData.wxData,
+          error: parsedCacheData.error,
+        },
+      }
+    }
+  } catch (err) {
+    console.log(KW_LOG_PREFIX + "Failure to Query Cache", err);
+    
     return {
       props: {
-        wxData: JSON.parse(value.toString('utf-8')),
-      },
+        wxData: {},
+        error: {
+          title: "Failure to Query Cache",
+          msg: "Unable to retrieve cached data. Please contact a site administrator to have the issue investigated."
+        }
+      }
     }
   }
 
-  let parsedWeatherData = {};
+  if (!dataInCache) {
+    console.log(KW_LOG_PREFIX + "Making External WL API Query");
 
-  if (err == null && !dataInCache) {
-    const res = await axios.get(
-      "https://api.weatherlink.com/v2/current/" + process.env.WEATHER_LINK_STATION_ID + "?api-key=" + process.env.WEATHER_LINK_API_KEY,
-      {
-        headers: {
-          "X-Api-Secret": process.env.WEATHER_LINK_API_SECRET,
+    try {
+      var res = await axios.get(
+        "https://api.weatherlink.com/v2/current/" + process.env.WEATHER_LINK_STATION_ID + "?api-key=" + process.env.WEATHER_LINK_API_KEY,
+        {
+          headers: {
+            "X-Api-Secret": process.env.WEATHER_LINK_API_SECRET,
+          }
+        }
+      );
+    } catch (err) {
+      console.log(KW_LOG_PREFIX + "Failure Making WL API Query", err)
+      
+      return {
+        props: {
+          wxData: {},
+          error: {
+            title: "Failure Making WL API Query",
+            msg: "Unable to query WeatherLink API for up-to-date data. Please contact a site administrator to have the issue investigated.",
+          }
         }
       }
-    );
+    }
 
-    parsedWeatherData = weatherLinkUtil.parseWeatherLinkAPIResponse(res.data);
+    try {
+      var parsedWeatherData = weatherLinkUtil.parseWeatherLinkAPIResponse(res.data);
 
-    mc.set(
-      KW_MEMCACHED_KEY,
-      JSON.stringify(parsedWeatherData),
-      { expires: KW_MEMCACHED_TIMEOUT_DURATION_IN_SECONDS }
-    );
+      let dataToCache = {
+        wxData: parsedWeatherData, 
+        error: error,
+      }
+
+      let { value } = await mc.set(
+        KW_MEMCACHED_KEY,
+        JSON.stringify(dataToCache),
+        { expires: KW_MEMCACHED_TIMEOUT_DURATION_IN_SECONDS }
+      );
+
+    } catch (error) {
+      console.log(KW_LOG_PREFIX + "Error Caching Data", err)
+      error = {
+        title: 'Error Caching Data',
+        msg: "Unable to cache WL data. Please contact a site administrator.",
+      }
+    }
   }
 
   return {
     props: {
       wxData: parsedWeatherData,
+      error: error,
     },
   }
 }
