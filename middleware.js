3df6a2ac3145d9ddb252ac7c255e01756a2f75d1
Michael Weiner <vikings412@gmail.com>
2023-08-16 20:14:54 -0500

    another attempt to have  middleware redirect http

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req, ev) {
-    if (process.env.NODE_ENV === 'production' && !req.headers.get('x-forwarded-proto').includes('https')) {
+    if (process.env.NODE_ENV === 'production' && (!req.headers.get('x-forwarded-proto').includes('https') || !req.headers.get('x-forwarded-proto') !== 'https')) {
         return NextResponse.redirect(`https://${req.headers.get('host')}${req.nextUrl.pathname}`, 301);
     }
 
