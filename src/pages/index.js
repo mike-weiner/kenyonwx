7209b18029c11a41d9d8128aaa5e63ac0fa3c2b9
Michael Weiner <vikings412@gmail.com>
2023-08-17 21:41:23 -0500

    adjust width of site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -90,7 +90,7 @@ export default function Homepage(props) {
       <div className="p-8 pb-24 space-y-8">
         <div className="md:flex md:items-center md:justify-between">
           <div className="min-w-0 flex-1">
-            <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight"
+            <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight max-w-fit"
               onClick={() => setOpenDebugSlideover(true)}
             >
               Kenyon, MN
