9a4a9dbacc15c73c7c34ca64ab88104eca387b4d
Michael Weiner <vikings412@gmail.com>
2024-10-04 20:53:39 -0500

    Remove Middleware No Longer Needed w/ Kamal Deployments

--- src/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(req, ev) {
-    if (process.env.NODE_ENV === 'production' && (!req.headers.get('x-forwarded-proto').includes('https'))) {
-        return NextResponse.redirect(`https://${req.headers.get('host')}${req.nextUrl.pathname}`, 301);
-    }
-
-    return NextResponse.next();
-}
