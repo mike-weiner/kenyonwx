2e7d13dc4cd4281d80a22e79f78ca79afde267fa
Michael Weiner <47571709+mike-weiner@users.noreply.github.com>
2024-10-04 19:43:53 -0500

    Health Check Page (#67)
    * Create Basic Health Check Page
    
    * add newline to EOF

---
+++ src/pages/up/index.js
@@ -0,0 +1,7 @@
+export default function Up() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-600">
+      <h1 className="text-white text-6xl font-bold">UP</h1>
+    </div>
+  );
+}
