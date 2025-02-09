e8bff916a0a807f3861c10ab76e1b8fe06aa3ed6
Michael Weiner <vikings412@gmail.com>
2025-02-09 14:09:59 -0600

    Add padding to page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ export default function Homepage(props) {
       <div className="p-8 pb-24 space-y-8">
         <div className="md:flex md:items-center md:justify-between">
           <div className="min-w-0 flex-1">
-            <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight max-w-fit"
+            <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight max-w-fit py-2"
               onClick={() => setOpenDebugSlideover(true)}
             >
               Kenyon, MN
