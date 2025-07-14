12b2f0ff2394e6001301eb47df2959d097fbd772
Michael Weiner <vikings412@gmail.com>
2025-07-13 20:42:55 -0500

    headlessui v2.0+ deprecation updates

--- src/components/Slideover/Wide/index.js
+++ src/components/Slideover/Wide/index.js
@@ -1,5 +1,5 @@
 import { Fragment, useEffect, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
 import { XCircleIcon } from '@heroicons/react/24/outline'
 
 export default function WideSlideover(props) {
@@ -10,14 +10,14 @@ export default function WideSlideover(props) {
   });
 
   return (
-    <Transition.Root show={open} as={Fragment}>
+    <Transition show={open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={props.onClose}>
         <div className="fixed inset-0" />
 
         <div className="fixed inset-0 overflow-hidden">
           <div className="absolute inset-0 overflow-hidden">
             <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
-              <Transition.Child
+              <TransitionChild
                 as={Fragment}
                 enter="transform transition ease-in-out duration-500 sm:duration-700"
                 enterFrom="translate-x-full"
@@ -26,13 +26,13 @@ export default function WideSlideover(props) {
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                <DialogPanel className="pointer-events-auto w-screen max-w-2xl">
                   <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                     <div className="px-4 sm:px-6">
                       <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
+                        <DialogTitle className="text-base font-semibold leading-6 text-gray-900">
                           {props.title || "Panel Title"}
-                        </Dialog.Title>
+                        </DialogTitle>
                         <div className="ml-3 flex items-center">
                           <button
                             type="button"
@@ -50,12 +50,12 @@ export default function WideSlideover(props) {
                       {props.children}
                     </div>
                   </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                </DialogPanel>
+              </TransitionChild>
             </div>
           </div>
         </div>
       </Dialog>
-    </Transition.Root>
+    </Transition>
   );
 }
